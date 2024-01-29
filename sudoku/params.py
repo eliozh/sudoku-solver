26dec06cae100c52cf4153fe2ebe9ac2b2837081
eliozh <eliozhou@outlook.com>
2024-01-29 17:07:35 +0800

    Collect all parameters in param.py

---
+++ sudoku/params.py
@@ -0,0 +1,22 @@
+# general parameters
+MIN_CELL_AREA = 100 * 100           # approximate minimum area of a single cell (in pixel)
+MAX_CELL_AREA = 150 * 150           # approximate maximum area of a single cell (in pixel)
+OPERATION_INTERVAL = 0.02           # time interval between two continuous operations
+LABELS_COORD = {
+    1: (72, 2046), 
+    2: (188, 2046), 
+    3: (308, 2046), 
+    4: (427, 2046), 
+    5: (533, 2046), 
+    6: (647, 2046), 
+    7: (770, 2046), 
+    8: (884, 2046), 
+    9: (1010, 2046)
+}                                   # coordinates of each label
+
+# event mode related parameters
+CONT_BTN_COORD = (545, 2145)        # coordinate of `continue` button
+PLAY_BTN_COORD = (547, 1645)        # coordinate of `play` button
+AFTER_DONE_INTERVAL = 5             # time to wait before tap `continue` button after game is done
+AFTER_CONT_INTERVAL = 1             # time to wait before tap `play` button after tap `continue` button
+AFTER_PLAY_INTERVAL = 2             # time to wait before capture screen shot after tap `play` button
